84c9af2a83f480505dc91b27672238165c79014d
anshi2310 <108393335+anshi2310@users.noreply.github.com>
2022-11-25 11:40:28 +0530

    Update variables.tf

--- quickstart/template/variables.tf
+++ quickstart/template/variables.tf
@@ -1,7 +1,49 @@
-# Use this file to define all variables that will be used in the configuraion 
+variable "agent_count" {
+  default = 3
+}
 
-variable "var1" {
-  type = string
-  description = "my first variable"
-  default = "foo"
+# The following two variable declarations are placeholder references.
+# Set the values for these variable in terraform.tfvars
+variable "aks_service_principal_app_id" {
+  default = "bfac5b08-bdce-460d-9ee8-02dd5a6144b7"
+}
+
+variable "aks_service_principal_client_secret" {
+  default = "WfS8Q~~QfVMTELuh4ipbIpwFyHm64_xWHbfQkbZB"
+}
+
+variable "cluster_name" {
+  default = "k8stest"
+}
+
+variable "dns_prefix" {
+  default = "k8stest"
+}
+
+# Refer to https://azure.microsoft.com/global-infrastructure/services/?products=monitor for available Log Analytics regions.
+variable "log_analytics_workspace_location" {
+  default = "eastus"
+}
+
+variable "log_analytics_workspace_name" {
+  default = "testLogAnalyticsWorkspaceName"
+}
+
+# Refer to https://azure.microsoft.com/pricing/details/monitor/ for Log Analytics pricing
+variable "log_analytics_workspace_sku" {
+  default = "PerGB2018"
+}
+
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
 }
